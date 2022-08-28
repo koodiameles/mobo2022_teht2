1f15cbdc88dfd4763e4b5286e8899eb4d1c3b7c0
Jussi Junnila <74765606+koodiameles@users.noreply.github.com>
2022-08-28 17:26:33 +0300

    teht 2 done

--- App.js
+++ App.js
@@ -1,38 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, View, Button, Alert, TextInput, Image, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Button, Alert, TextInput, Text  } from 'react-native';
 
 export default function App() {
 
-  const [number1, setNumber1] = useState("");
-  const [number2, setNumber2] = useState("");
-  const [result, setResult] = useState("");
+  const [playerGuess, setPlayerGuess] = useState("");
+  const [playerGuessCount, setPlayerGuessCount] = useState(0);
+  const [gameText, setGameText] = useState("Guess a number between 1-100");
+  const [winningNumber, setWinningNumber] = useState(0);
 
-  const plus = () => { 
-    let summation = Number(number1) + Number(number2);
-    setResult(summation)
-  };
-  const minus = () => { 
-    let deduction = Number(number1) - Number(number2);
-    setResult(deduction)
-  };
+  useEffect(() => {
+    let randomNumber = Math.floor(Math.random() * 100) + 1;
+    setWinningNumber(randomNumber);
+  }, [])
 
+
+  const guess = () => { 
+    var updatedQuessCount = playerGuessCount + 1;
+    setPlayerGuessCount(updatedQuessCount)
+
+    var guess = parseInt(playerGuess);
+    var targetNumber = parseInt(winningNumber);
+    var newText = "";
+    if (guess > targetNumber) {
+      newText = "Your quess " + playerGuess + " is too high!";
+      setGameText(newText);
+      setPlayerGuess("")
+    } else if (guess < targetNumber) {
+      newText = "Your quess " + playerGuess + " is too low!";
+      setGameText(newText);
+      setPlayerGuess("")
+    } else {
+      setGameText("CORRECT!");
+      Alert.alert("You win! " + "(it took " + playerGuessCount + " guesses)"   );
+    }
+  };
 
   return (
     <>
       <View style={styles.containerHeader}>
-        <Text style={styles.assignemtHeaderText}>TEHT 1 LASKIN</Text>
+        <Text style={styles.assignemtHeaderText}>TEHT 2</Text>
+        <Text style={styles.assignemtHeaderText}>NUMERON ARVAUS</Text>
       </View>
       <View style={styles.container}>
-        <Text style={{color:"white"}}>Result: {result}</Text>
-        <TextInput keyboardType="numeric" style={styles.input} onChangeText={setNumber1} value={number1}/>
-        <TextInput keyboardType="numeric" style={styles.input} onChangeText={setNumber2} value={number2}/>
+        {/* <Text style={{color:"white"}}>guess: {playerGuess}</Text> */}
+        {/* <Text style={{color:"white"}}>count: {playerGuessCount}</Text> */}
+        {/* <Text style={{color:"white"}}>target number:{winningNumber}</Text> */}
+        <Text style={{color:"white"}}>{gameText}</Text>
+        <TextInput keyboardType="numeric" style={styles.input} onChangeText={playerGuess => setPlayerGuess(playerGuess)} value={playerGuess}/>
         <View style={{display: 'flex', flexDirection: 'row', margin: 10}}>
-          <View style={{flex: 1, marginHorizontal: 20}}>
-            <Button color="green" onPress={plus} title="+" />
-          </View>
-          <View style={{flex: 1, marginHorizontal: 20}}>
-            <Button color="red" onPress={minus} title="-" />
+          <View style={{flex: 1, marginHorizontal: 60}}>
+            <Button color="purple" onPress={guess} title="Make Guess" />
           </View>
         </View>
         <StatusBar style="auto" />
